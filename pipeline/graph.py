85c4162ae83e527b6c70d3957819924f7d5ca356
udayraj1238 <rajuday6002@gmail.com>
2025-11-23 13:48:00 +0530

    Build pipeline graph - part 6

--- pipeline/graph.py
+++ pipeline/graph.py
@@ -4,3 +4,4 @@ from pipeline.state import RAGState
 from pipeline.nodes.retrieve import retrieve_node
 from pipeline.nodes.grade_relevance import grade_relevance_node
 from pipeline.nodes.rewrite_query import rewrite_query_node
+from pipeline.nodes.generate import generate_node
