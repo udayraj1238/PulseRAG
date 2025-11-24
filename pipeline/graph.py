9c6e9b8368fed34764d6a6b0605a09269684c747
udayraj1238 <rajuday6002@gmail.com>
2025-11-24 10:57:00 +0530

    Build pipeline graph - part 8

--- pipeline/graph.py
+++ pipeline/graph.py
@@ -6,3 +6,4 @@ from pipeline.nodes.grade_relevance import grade_relevance_node
 from pipeline.nodes.rewrite_query import rewrite_query_node
 from pipeline.nodes.generate import generate_node
 from pipeline.nodes.score_hallucination import score_hallucination_node
+
