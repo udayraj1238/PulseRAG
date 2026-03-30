2ad58155bd26519ff28e8036dbb18b2ef8508a9b
udayraj1238 <rajuday6002@gmail.com>
2026-03-30 09:45:00 +0530

    Implement text embedder - part 14

--- ingestion/embedder.py
+++ ingestion/embedder.py
@@ -12,3 +12,4 @@ def embed_text(text: str) -> List[float]:
     return vector.tolist()
 
 def embed_batch(texts: List[str], batch_size: int = 64) -> List[List[float]]:
+    '''Embed a list of strings in batches.'''
