bb5a9495d559f478798254bd842082f4ad2b3056
udayraj1238 <rajuday6002@gmail.com>
2026-04-21 10:01:00 +0530

    Implement semantic cache logic - part 46

--- cache/semantic_cache.py
+++ cache/semantic_cache.py
@@ -44,3 +44,4 @@ class SemanticCache:
             
             # Cosine similarity (vectors are already normalized)
             similarity = float(np.dot(query_vector, cached_vector))
+            
