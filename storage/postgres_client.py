ae76c8f58a8acc884e1e31843ebf2a51a0bd6842
udayraj1238 <rajuday6002@gmail.com>
2026-05-01 15:53:00 +0530

    Update postgres_client.py logic - part 3

--- storage/postgres_client.py
+++ storage/postgres_client.py
@@ -3,3 +3,5 @@ CREATE_TABLES_SQL = '''
 CREATE TABLE IF NOT EXISTS conversations (
     id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     query           TEXT NOT NULL,
+    rewritten_query TEXT,
+    answer          TEXT NOT NULL,
